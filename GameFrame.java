4e50c00dfef3cf3c6929fe01542ba7f125d69ac0
koguls <kogulkogul4@gmail.com>
2024-07-28 11:59:34 +0530

    Jpanel created a 8 class

--- GameFrame.java
+++ GameFrame.java
@@ -9,6 +9,9 @@ public class GameFrame extends JFrame {
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setResizable(false);
         this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+        
     }
     
 }
--- GamePanel.java
+++ GamePanel.java
@@ -5,6 +5,39 @@ import javax.swing.JPanel;
 
 
 public class GamePanel extends JPanel implements ActionListener {
+
+    GamePanel(){
+
+    }
+
+    public void startGame(){
+        
+    }
+    
+    public void paintComponent(){
+
+    }
+
+    public void draw(){
+        
+    }
+
+    public void appleEat(){
+
+    }
+
+    public void move(){
+
+    }
+    public void checkApple(){
+
+    }
+    public void chechCollisions(){
+
+    }
+    public void gameOver(){
+
+    }
     
     @Override 
     public void actionPerformed(ActionEvent e){
