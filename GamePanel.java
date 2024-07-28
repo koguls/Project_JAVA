f06237f32f96f3ff13e23867c557d9580321cdd4
koguls <kogulkogul4@gmail.com>
2024-07-28 11:49:25 +0530

    JFrame

---
+++ GamePanel.java
@@ -0,0 +1,15 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+
+
+public class GamePanel extends JPanel implements ActionListener {
+    
+    @Override 
+    public void actionPerformed(ActionEvent e){
+
+
+    }
+    
+}
