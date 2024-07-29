64c33407be19da5c2c156c68c956ea7e554aba5f
koguls <kogulkogul4@gmail.com>
2024-07-29 11:05:49 +0530

    checkapple()

--- GamePanel.java
+++ GamePanel.java
@@ -111,6 +111,12 @@ public class GamePanel extends JPanel implements ActionListener {
 
     }
     public void checkApple(){
+        if ((x[0] == appleX) && (y[0] == appleY)) {
+            bodyparts++;
+            applesEaten++;
+            newapple();
+            
+        }
 
     }
     public void chechCollisions(){
